12bd8fff60329e8f58534f50499e4002c2b5638e
Oluwaz <108075598+Oluwaz@users.noreply.github.com>
2024-06-15 14:49:01 +0000

    Raw-HTML-QuestionPage

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,24 +1,49 @@
 import { useLocation } from "react-router-dom";
-import data from '../starter-code/data.json';
+import { useState } from "react";
+import data from "../starter-code/data.json";
+import "./Question.css";
 
 function Question() {
-    const location = useLocation();
-    const {index} = location.state;
+  const location = useLocation();
+  const { index } = location.state;
 
-    const assessmentObj = data["quizzes"][index]
-    const questionObj = assessmentObj["questions"]
-    console.log(questionObj);
+  const assessmentObj = data["quizzes"][index];
+  const questionObj = assessmentObj["questions"];
+  const answerOdj = assessmentObj["options"];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
-    return (
-        <div>
-            Look at me, I'm the questions
-            {index == 0 ? <div>This is HTML questions</div> : null}
-            <div>
-                <div>Question</div>
-                <div>Answers</div>
-            </div>
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [showxt, setText] = useState(false);
+  
+
+  return (
+    <div className="container">
+      <div className="initial">
+        <div className="question-count">
+          <span>Question {currentQuestion + 1}</span> of {questionObj.length}
+          <div className="question-text">
+            {questionObj[currentQuestion].question}
+          </div>
         </div>
-    )
+        <div class="progress-container">
+          <div class="progress-bar">
+                ffffffffff
+          </div>
+        </div>
+      </div>
+
+        <div className="answers-section">
+            <ul>
+                <li>MOdem</li>
+                <li>Lan</li>
+                <li>Pasword</li>
+                <li>sdddd</li>
+            </ul>
+            <button id="nextQuestion">Next Question</button>
+        </div>
+    </div>
+  );
 }
 
 export default Question;
--- src/components/welcomepage/welcome.jsx
+++ src/components/welcomepage/welcome.jsx
@@ -6,12 +6,13 @@ import htmlicon1 from "../../starter-code/assets/images/icon-html.svg";
 import htmlicon2 from "../../starter-code/assets/images/icon-css.svg";
 import htmlicon3 from "../../starter-code/assets/images/icon-js.svg";
 import htmlicon4 from "../../starter-code/assets/images/icon-accessibility.svg";
+import background from "../../starter-code/assets/images/pattern-background-desktop-light.svg";
 
-import "./welcome.css";
+
 
 const Welcome = () => {
   return (
-    <div className="Welcome">
+    <section className="Welcome">
       <div className="Initial">
         <h1 className="initial-h1">
           <span id="own">Welcome to the </span> <br /> Frontend Quiz!
@@ -22,32 +23,38 @@ const Welcome = () => {
       <div className="column-container">
         <Link id="link" to="/questions" state={{ index: 0 }}>
           <div className="column-item">
-
-            <div style={{background: '#fff1e9'}} className="img-div"><img className="img-out" src={htmlicon1} /></div>
-            
+            <div style={{ background: "#fff1e9" }} className="img-div">
+              <img className="img-out" src={htmlicon1} />
+            </div>
             HTMLS
           </div>
         </Link>
         <Link id="link" to="/questions" state={{ index: 1 }}>
           <div className="column-item">
-          <div style={{background: '#e0fdff'}}  className="img-div"><img className="img-out" src={htmlicon2} /></div>
+            <div style={{ background: "#e0fdff" }} className="img-div">
+              <img className="img-out" src={htmlicon2} />
+            </div>
             CSS
           </div>
         </Link>
         <Link id="link" to="/questions" state={{ index: 2 }}>
           <div className="column-item">
-          <div style={{background: '#ebf0ff'}}  className="img-div"><img className="img-out" src={htmlicon3} /></div>
+            <div style={{ background: "#ebf0ff" }} className="img-div">
+              <img className="img-out" src={htmlicon3} />
+            </div>
             JavaScript
           </div>
         </Link>
         <Link id="link" to="/questions" state={{ index: 3 }}>
           <div className="column-item">
-          <div style={{background: '#f6e7ff'}}  className="img-div"><img className="img-out" src={htmlicon4} /></div>
+            <div style={{ background: "#f6e7ff" }} className="img-div">
+              <img className="img-out" src={htmlicon4} />
+            </div>
             ACCESSIBILITY
           </div>
         </Link>
       </div>
-    </div>
+    </section>
   );
 };
 
